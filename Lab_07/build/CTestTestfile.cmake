ff65c4c760323a0084597e37d2043d69e0d467df
dmitrijmrsh <kapustinrudolf@gmail.com>
2023-12-24 15:27:15 +0300

    build dir delete

--- Lab_07/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitrijmrsh/Labs-git/OOP-Labs/Lab_07
-# Build directory: /home/dmitrijmrsh/Labs-git/OOP-Labs/Lab_07/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BASIC "/home/dmitrijmrsh/Labs-git/OOP-Labs/Lab_07/build/testing")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/dmitrijmrsh/Labs-git/OOP-Labs/Lab_07/CMakeLists.txt;17;add_test;/home/dmitrijmrsh/Labs-git/OOP-Labs/Lab_07/CMakeLists.txt;0;")
